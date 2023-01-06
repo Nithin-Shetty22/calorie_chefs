fe9e149bdc72067716c9bb3b964dbab6ede05a96
Nithin Shetty <nithin_shetty@Dineroai.com>
2023-01-06 13:50:03 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import {BrowserRouter as Router,Switch,Route} from "react-router-dom";
+import {Hero, Footer} from './components';
+import Home from './pages/Home/Home';
+import About from './pages/About/About';
+import Recipe from './pages/Recipe/Recipe';
+import Order from './pages/Order/Order';
+import GlobalStyles from './Globalstyles';
+function App() {
+  return (
+    <Router>
+      <GlobalStyles/>
+      <Hero/>
+      <Switch>
+        <Route exact path="/" component={Home}/>
+        <Route exact path="/about" component={About}/>
+        <Route exact path="/recipe" component={Recipe}/>
+        <Route exact path="/order-now" component={Order}/>
+      </Switch>
+      <Footer/>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,14 @@
+import {Works, Welcome, Recipes, Meals} from '../../components';
+
+function Home() {
+    return (
+        <div>
+            <Works/>
+            <Welcome/>
+            <Recipes/>
+            <Meals/>
+        </div>
+    )
+}
+
+export default Home;
